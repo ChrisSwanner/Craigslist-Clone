66e357ba6006bd9d1b0cd171a0a42bbf98d4fb49
Chris Swanner <cmanswanner@gmail.com>
2018-03-30 13:28:43 -0700

    add failing filter

--- src/app/sales/sales.component.ts
+++ src/app/sales/sales.component.ts
@@ -9,10 +9,13 @@ import { Sale } from '../sale.model';
 export class SalesComponent implements OnInit {
   sales: Sale[] = [
     new Sale('Used Bike', 'I have had it for 20 years, its old and rusty', 100)
-  ]
+  ];
+
+  filteredSales: Sale[] = [];
 
   // currentRoute: string = this.router.url;
   public addingSale: boolean = false;
+  public saleFilter: boolean = false;
 
   addNewSale() {
     this.addingSale = true;
@@ -25,8 +28,18 @@ export class SalesComponent implements OnInit {
 
   submitForm(title: string, description: string, price: number) {
     this.addingSale = false;
-    let newSale: Sale = new Sale(title,description,price);
+    let newSale: Sale = new Sale(title,description,price,category);
     this.sales.push(newSale);
+  }
+
+  filterSales(category) {
+    this.filteredSales = [];
+    this.saleFilter = true;
+    for (let i = 0; i < this.sales.length; i++) {
+      if(this.sales[i].category === category) {
+        this.filteredSales.push(this.sales[i]);
+      }
+    }
   }
 
 }
